328bc364a45441a561c1b60647ec0b70e3eba9ec
sabirmostofa <sabirmostofa@gmail.com>
2023-07-13 03:44:57 +0200

    both schemas working after timeout, sleep added

--- lqb/docker-entrypoint.sh
+++ lqb/docker-entrypoint.sh
@@ -4,7 +4,7 @@ echo "########FROM THE ENTRYPOINT!################"
 
 #keep the container running
 
-for d in ./changelog/changelogs ; do (cd "$d" && ls -al); done
+for d in ./changelog/changelogs/* ; do (cd "$d" && ls -al && liquibase update --changelog-file=mainChangeLog.xml && sleep 2); done
 tail -f /dev/null
 
 
