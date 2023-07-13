4907c7f14077df8f2c1f35440911c09c30ba30e0
sabirmostofa <sabirmostofa@gmail.com>
2023-07-13 03:51:09 +0200

    script

--- rm.bat
+++ rm.bat
@@ -1,2 +1,3 @@
 powershell -Command "& {docker rm -f $(docker ps -a -q);}"
 powershell -Command "& {docker volume rm -f $(docker volume ls -q);}"
+@REM powershell -Command "& {docker network rm  $(docker network ls -q);}"
